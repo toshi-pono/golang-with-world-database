6738d61e2f434cf924b9407124b554f1a5d024f1
toshi-pono <toshi.pono.h@gmail.com>
2021-05-29 00:12:19 +0900

    都市のデータを返すエンドポイントを追加

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/jmoiron/sqlx v1.3.4 // indirect
+	github.com/labstack/echo/v4 v4.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/labstack/echo/v4"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
@@ -19,20 +22,32 @@ type City struct {
 	Population  int    `json:"population,omitempty" db:"Population"`
 }
 
-func main() {
-	db, err := sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOSTNAME"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
+var (
+	db *sqlx.DB
+)
 
+func main() {
+	_db, err := sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOSTNAME"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
 	if err != nil {
 		log.Fatalf("Cannot Connect to Database: %s", err)
 	}
 
-	fmt.Println("Connected!")
+	db = _db
+
+	e := echo.New()
+	e.GET("/cities/:cityName", getCityInfoHandler)
+	e.Start(":" + os.Getenv("API_PORT"))
+}
+
+func getCityInfoHandler(c echo.Context) error {
+	cityName := c.Param("cityName")
+	fmt.Println(cityName)
+
 	var city City
-	if err := db.Get(&city, "SELECT * FROM city WHERE NAME='TOKYO'"); errors.Is(err, sql.ErrNoRows) {
-		log.Println("no such city Name=%s", "Tokyo")
+	if err := db.Get(&city, "SELECT * FROM city WHERE Name=?", cityName); errors.Is(err, sql.ErrNoRows) {
+		log.Printf("No Such City Name=%s", cityName)
 	} else if err != nil {
 		log.Fatalf("DB Error: %s", err)
 	}
-	fmt.Printf("Tokyoの人口は%d人です\n", city.Population)
-
+	return c.JSON(http.StatusOK, city)
 }
